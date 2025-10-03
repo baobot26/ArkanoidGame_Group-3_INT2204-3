c60f5adb8497e9e4dded0306f2446d99424b2c57
baobot26 <tranducbao12012006@gmail.com>
2025-10-03 13:35:28 +0700

    [BaoTD] feat: add base class templates for the project

---
+++ src/src/Ball.java
@@ -0,0 +1,20 @@
+/**
+ * Ball class represents a ball object in the game.
+ * It extends the MoveableObject class to inherit movement properties.
+ */
+public class Ball extends MoveableObject{
+    @Override
+    void move() {
+
+    }
+
+    @Override
+    void update() {
+
+    }
+
+    @Override
+    void render() {
+
+    }
+}
---
+++ src/src/Brick.java
@@ -0,0 +1,16 @@
+/**
+ * Class representing a brick in the game.
+ * It extends the GameObject class to inherit position and dimension properties.
+ */
+public class Brick extends GameObject{
+
+    @Override
+    void update() {
+
+    }
+
+    @Override
+    void render() {
+
+    }
+}
